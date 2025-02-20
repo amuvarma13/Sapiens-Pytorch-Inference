46f2403f228269cfcee5b6efa1015f9a6fff7cf4
Amartya Varma <amartya.varma@gmail.com>
2025-02-20 00:30:17 -0800

    f

--- upload_f.py
+++ upload_f.py
@@ -3,7 +3,7 @@ import os
 from concurrent.futures import ThreadPoolExecutor
 from tqdm import tqdm
 
-base_repo_id = "outputs/"
+base_repo_id = "output_images/"
 upload_name = "amuvarma/video_sapiens"
 
 
